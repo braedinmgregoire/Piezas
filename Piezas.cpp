9eea34e3f7b63cfe97389b15e2671bbc82e3cb1e
braedinmgregoire <braedinmgregoire@gmail.com>
2020-12-11 09:18:18 -0800

    Added  initial functionality to constructor, pieceAt, reset, dropPiece

--- Piezas.cpp
+++ Piezas.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 /** CLASS Piezas
  * Class for representing a Piezas vertical board, which is roughly based
- * on the game "Connect Four" where pieces are placed in a column and 
+ * on the game "Connect Four" where pieces are placed in a column and
  * fall to the bottom of the column, or on top of other pieces already in
  * that column. For an illustration of the board, see:
  *  https://en.wikipedia.org/wiki/Connect_Four
@@ -17,11 +17,17 @@
 
 
 /**
- * Constructor sets an empty board (default 3 rows, 4 columns) and 
+ * Constructor sets an empty board (default 3 rows, 4 columns) and
  * specifies it is X's turn first
 **/
 Piezas::Piezas()
 {
+  for(int i=0;i<BOARD_ROWS;i++){
+    for(int j=0;j<BOARD_COLS;j++){
+      board[i][j] = Blank;
+    }
+  }
+  turn = X;
 }
 
 /**
@@ -30,18 +36,27 @@ Piezas::Piezas()
 **/
 void Piezas::reset()
 {
+  for(int i=0;i<BOARD_ROWS;i++){
+    for(int j=0;j<BOARD_COLS;j++){
+      board[i][j] = Blank;
+    }
+  }
+  return;
 }
 
 /**
  * Places a piece of the current turn on the board, returns what
- * piece is placed, and toggles which Piece's turn it is. dropPiece does 
+ * piece is placed, and toggles which Piece's turn it is. dropPiece does
  * NOT allow to place a piece in a location where a column is full.
- * In that case, placePiece returns Piece Blank value 
+ * In that case, placePiece returns Piece Blank value
  * Out of bounds coordinates return the Piece Invalid value
  * Trying to drop a piece where it cannot be placed loses the player's turn
-**/ 
+**/
 Piece Piezas::dropPiece(int column)
 {
+  if(column < 0 || column >= BOARD_COLS){
+    return Invalid;
+  }
     return Blank;
 }
 
@@ -51,13 +66,18 @@ Piece Piezas::dropPiece(int column)
 **/
 Piece Piezas::pieceAt(int row, int column)
 {
-    return Blank;
+  if((row < 0 || row>=BOARD_ROWS) || (column < 0 || column >= BOARD_COLS)){
+    return Invalid;
+  }
+  else{
+    return board[row][column];
+  }
 }
 
 /**
  * Returns which Piece has won, if there is a winner, Invalid if the game
  * is not over, or Blank if the board is filled and no one has won ("tie").
- * For a game to be over, all locations on the board must be filled with X's 
+ * For a game to be over, all locations on the board must be filled with X's
  * and O's (i.e. no remaining Blank spaces). The winner is which player has
  * the most adjacent pieces in a single line. Lines can go either vertically
  * or horizontally. If both X's and O's have the same max number of pieces in a
